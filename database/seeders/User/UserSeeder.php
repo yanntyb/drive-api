f42f92127deacef5d1c89fbcbef3b48c5e1435e7
yanntyb <yanntyb.lbc@gmail.com>
2022-11-17 14:10:02 +0100

    wip storage factory

--- database/seeders/User/UserSeeder.php
+++ database/seeders/User/UserSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders\User;
 
+use App\Models\Storage\Storage;
 use App\Models\User;
 use Database\Seeders\User\Role\RoleSeeder;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
+use Str;
 
 class UserSeeder extends Seeder
 {
@@ -28,11 +29,17 @@ class UserSeeder extends Seeder
 
         User::factory()
             ->count(10)
+            ->has(
+                Storage::factory()
+                    ->count(1)
+                    ->state(function (array $attributes, User $user) {
+                        return ["path" => Str::slug($user->name . " " . uniqid("storage", true))];
+                    })
+            )
             ->create()
-            ->each(function(User $user) use ($usersRoles){
+            ->each(function (User $user) use ($usersRoles) {
                 $user->assignRole($usersRoles);
-            })
-        ;
+            });
 
     }
 }
